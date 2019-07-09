5b5b8f7b3a6527063797beee4c52349ee5306422
Bruno Martins <bgcngm@gmail.com>
2019-07-09 22:31:00 +0100

    Doze: Migrate to androidx
    Change-Id: Ifaa67283a3437549a2304088a0ce377b399da19f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- doze/src/org/lineageos/settings/doze/Utils.java
+++ doze/src/org/lineageos/settings/doze/Utils.java
@@ -20,7 +20,7 @@ package org.lineageos.settings.doze;
 import android.content.Context;
 import android.content.Intent;
 import android.os.UserHandle;
-import android.support.v7.preference.PreferenceManager;
+import androidx.preference.PreferenceManager;
 import android.provider.Settings;
 import android.util.Log;
 
