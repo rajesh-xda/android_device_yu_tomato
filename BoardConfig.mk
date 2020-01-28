0306ed3d8c06816b1048fd8acbf74182f92a0125
Rajesh <46389111+rajesh-xda@users.noreply.github.com>
2020-01-28 23:02:17 +0530

    Switch to enforcing build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,7 +61,6 @@ LZMA_RAMDISK_TARGETS := recovery
 TARGET_KERNEL_CONFIG := lineageos_tomato_defconfig
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # LineageHW
 JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(DEVICE_PATH)/lineagehw|**/*.java
