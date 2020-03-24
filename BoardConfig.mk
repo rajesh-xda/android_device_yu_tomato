1e55b48af55328a91724079a13ef44897825c338
Rajesh <46389111+rajesh-xda@users.noreply.github.com>
2020-03-24 10:27:18 +0530

    Set target for double tap to wake node

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,7 +57,7 @@ TARGET_KERNEL_CONFIG := lineageos_tomato_defconfig
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Power
-TARGET_POWER_SET_FEATURE_LIB := libpower_set_feature_tomato
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/touchscreen/touchscreen_dev/gesture_ctrl"
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
