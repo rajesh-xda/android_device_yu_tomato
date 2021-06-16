2703f121a0c939805cb7c7e12492a9287540b803
Rajesh R <rajeshramece@gmail.com>
2021-06-16 19:16:03 +0530

    Revert "tomato:Add MIUI Poweroff Charging Animation"
    This reverts commit 16ad16e17899d4884f2a0f1f4b822d96fc5be394.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,10 +66,6 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 # Power
 TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
-# Offline Charging
-BOARD_CHARGER_DISABLE_INIT_BLANK := true
-BOARD_CHARGER_ENABLE_SUSPEND := true
-
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
