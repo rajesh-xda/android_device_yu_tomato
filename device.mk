1d0f73b52be2bcd8d54055d80747dec652e18428
Rajesh R <rajeshramece@gmail.com>
2021-06-25 08:33:07 +0530

    Camera : Clean dead targets and update

--- device.mk
+++ device.mk
@@ -59,12 +59,13 @@ TARGET_SCREEN_WIDTH := 720
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8916 \
-    libmm-qcamera \
+    libshim_camera \
     Snap \
     Recorder
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl 
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl
 
 # Due to multi-density builds, these are set by init
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model ro.sf.lcd_density
